755f336ef90560d3a917fde1ea9b928d1676abc8
Irvin Torres <irvin7torres7@gmail.com>
2025-06-03 20:20:44 -0700

    added getAndDisplayJoke that will fetch and joke and display on screen / currently can fetch and console log joke

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 
-// var theJoke = [];
 
-// function gotJokes() {
-//     fetch("https://api.chucknorris.io/jokes/random")
-//         .then(response => response.json())
-//         .then(data =>   {
-//             theJoke.push(data.value);
-//             console.log(theJoke)
-            
-//         })
-//         .catch(error => console.error('Error fetching data:', error));
-// }
+async function getAndDisplayJoke() {
+    const url = "https://api.chucknorris.io/jokes/random"
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+        const data = await json.value;
+        console.log(data);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
 
