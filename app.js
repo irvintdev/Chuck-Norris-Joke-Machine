afaaf8f2bcf2aa0de58a741cbdb8bc85293cde7f
Irvin Torres <irvin7torres7@gmail.com>
2025-06-03 20:05:07 -0700

    added a button generator button / added id 'Joke' to h2 tags

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 
-function gotJokes() {
-    fetch("https://api.chucknorris.io/jokes/random")
-        .then(response => response.json())
-        .then(data => console.log(data.value))
-}
+// var theJoke = [];
+
+// function gotJokes() {
+//     fetch("https://api.chucknorris.io/jokes/random")
+//         .then(response => response.json())
+//         .then(data =>   {
+//             theJoke.push(data.value);
+//             console.log(theJoke)
+            
+//         })
+//         .catch(error => console.error('Error fetching data:', error));
+// }
+
